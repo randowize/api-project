23639afd78aaa0876a27834e78fd76694b2825b9
randowize <lamine1905@gmail.com>
2017-06-07 13:27:51 +0000

    submission

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
 var http = require('http');
 var express = require('express');
-
+var moment = require('moment');
 
 var app = express();
 var server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.json('hello world');
+app.get('/:timestamp?', (req, res) => {
+    var timestamp = req.params.timestamp;
+    var unix = null, natural = null;
+    if(timestamp){
+       var ts = parseInt(timestamp, 10);
+       var m = (isNaN(ts)) ? moment(timestamp) : moment.unix(ts);
+       unix = m.unix();
+       natural = m.format('MMMM DD, YYYY');
+    }
+    return res.json({
+        natural:natural,
+        unix:unix
+    });
 });
 
 server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("timestap server runnint at ", addr.address + ":" + addr.port);
 });
